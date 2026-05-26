46be3abbc6c16a35aa5f547d0a758106d2a0ba69
vsnaipdms <vsn.aipdms@gmail.com>
2026-05-26 20:40:47 +0530

    fixed homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,4 +4,14 @@ export default function Home() {
       Background Remover Working 🚀
     </div>
   );
+}  GNU nano 9.0                    src/app/page.tsx
+export default function Home() {
+  return (
+    <div style={{padding:"40px",fontSize:"30px"}}>
+      Background Remover Working  ~@
+    </div>
+  );
 }
+
+
+
