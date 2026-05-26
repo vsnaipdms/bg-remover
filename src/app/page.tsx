c5b2b83100fe9894baab13da914616f16d5b7d80
vsnaipdms <vsn.aipdms@gmail.com>
2026-05-26 20:54:19 +0530

    fixed jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 export default function Home() {
   return (
-    <div style={{padding:"40px",fontSize:"30px"}}>
+    <div style={{ padding: "40px", fontSize: "30px" }}>
       Background Remover Working 🚀
     </div>
   );
-}  GNU nano 9.0                    src/app/page.tsx
-export default function Home() {
-  return (
-    <div style={{padding:"40px",fontSize:"30px"}}>
-      Background Remover Working  ~@
-    </div>
-  );
 }
-
-
-
